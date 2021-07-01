bca31007c45e651650706642618164cc32d45c2e
Ruben <rubentrevino95@gmail.com>
2021-07-01 12:46:26 -0500

    styling

--- python/openMain.py
+++ python/openMain.py
@@ -1,7 +1,7 @@
 import os
 import openai
-# import keys from keys.py
+from keys import keys
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv(keys)
 
 response = openai.Completion.create(engine="davinci", prompt="This is a test", max_tokens=5)
